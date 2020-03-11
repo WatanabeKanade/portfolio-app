fc3b88c3d1d76b2410bb1c3882a736f28e412921
kanade <futae.0328@gmail.com>
2020-03-12 08:59:27 +0900

    fix download.js

--- src/store/modules/download.js
+++ src/store/modules/download.js
@@ -3,8 +3,8 @@ import "firebase/firestore";
 
 
 const state = {
-  monochromeItems: {},
-  colorItems: {}
+  monochromeItems: { "first_photo": "https://firebasestorage.googleapis.com/v0/b/portfolio-app-9ccdd.appspot.com/o/photos%2Fmonochromes%2FF329895C-34A4-4EF0-89BA-5D7F915830CF.jpeg?alt=media&token=3c865b3e-877c-4afb-a120-6ce1db51205f" },
+  colorItems: { "first_photo": "https://firebasestorage.googleapis.com/v0/b/portfolio-app-9ccdd.appspot.com/o/photos%2Fcolors%2Fcolor_2019-3.jpg?alt=media&token=15a2bb70-7baf-4161-ac8b-6a7e5ca9df1f" }
 }
 
 const actions = {
